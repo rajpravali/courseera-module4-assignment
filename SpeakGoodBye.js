c8a742c16b89230348e58dbae686914da932bcd5
rajpravali <52438472+rajpravali@users.noreply.github.com>
2020-06-09 19:17:36 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,10 +1,8 @@
-(function (window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = {}
-	byeSpeaker.speak = function(name) {
-	  console.log(speakWord + " " + name);
-	}
-
-	window.byeSpeaker = byeSpeaker;
-
+(function(window) {
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
 })(window);
